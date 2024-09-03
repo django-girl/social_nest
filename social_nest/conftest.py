c0719d7d678113a308afa0baf7022c55eb18c4bd
Priyanka Garg <priyankagarg@aquevix.com>
2024-09-03 21:16:19 +0530

    Initial commit.

---
+++ social_nest/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from social_nest.users.models import User
+from social_nest.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
