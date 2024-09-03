56bae2f139b9597fe64456a0b29df86a5c605efb
Priyanka Garg <k.priyagarg097@gmail.com>
2024-09-03 22:14:47 +0530

    Adding friends app.

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "social_nest.users",
+    "friends",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
